4ccf29a2c46103b72cd055f394c46e9697b91ad1
soudey123 <38169031+soudey123@users.noreply.github.com>
2018-04-16 21:46:06 -0500

    Update test_farsdata.R
    Commit new changes

--- tests/testthat/test_farsdata.R
+++ tests/testthat/test_farsdata.R
@@ -11,10 +11,3 @@ test_that('Loads multiple years correctly', {
         expect_equal(length(dfs), 3)
 })
 
-test_that('FARS Summarize Years is correct', {
-  years <- 2013:2015
-  df <- fars_summarize_years(years)
-  expect_that(nrow(df), equals(12))
-  expect_that(ncol(df), equals(length(years) + 1))
-  expect_that(names(df)[1], matches('MONTH'))
-})
