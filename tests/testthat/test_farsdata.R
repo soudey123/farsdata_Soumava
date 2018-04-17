3203b6300591d4491a214d64af8188c8467b5cf3
soudey123 <38169031+soudey123@users.noreply.github.com>
2018-04-16 21:03:10 -0500

    Update test_farsdata.R
    Modify test options

--- tests/testthat/test_farsdata.R
+++ tests/testthat/test_farsdata.R
@@ -1,12 +1,20 @@
-context('reading and summarizing data')
+context('reading data')
 
 test_that('Simple test', {
   testthat::expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
 })
 
-test_that("import the fars data", {
-  data_path <- system.file("extdata", "accident_2013.csv.bz2", package = "farsdata")
-  dat       <- fars_read(data_path)
-  testthat::expect_that(dat, is_a('tbl_df'))
-  testthat::expect_equal(nrow(dat), 30202)
+context('File load and summary are correct')
+test_that('Loads multiple years correctly', {
+        dfs <- fars_read_years(2013:2015)
+        expect_that(dfs, is_a('list'))
+        expect_that(dfs[[1]], is_a('tbl_df'))
+        expect_equal(length(dfs), 3)
+})
+
+test_that('Reads one file correctly', {
+        fn <- make_filename(2013)
+        df <- fars_read(fn)
+        expect_that(df, is_a('tbl_df'))
+        expect_that(nrow(df), is_more_than(0))
 })
